e2c6aee6574181bfb970ac99bb3f574c0db42d8a
Arthur Rodrigues de Souza <arthurrodriguesdesouza66@gmail.com>
2024-06-16 18:07:22 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const formatCurrency = (number) => {
+    return number.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+}
+
+const getProducts = async () => {
+    const response = await fetch('assets/js/products.json');
+    const data = await response.json();
+    return data;
+}
+
+const generateCard = async () => {
+    const products = await getProducts();
+    products.map(product => {
+        let card = document.createElement('div');
+        card.classList.add('card__produto');
+        card.innerHTML = `
+        <figure>
+            <img src="assets/images/${product.image}" alt="${product.product_name}">
+        </figure>
+        <div class="card__produto_detalhes">
+            <h4>${product.product_name}</h4>
+            <h5>${product.product_model}</h5>
+        </div>
+        <h6>${formatCurrency(product.price)}</h6>`;
+        const listaProdutos = document.querySelector('.lista__produtos');
+        listaProdutos.appendChild(card)
+    });
+}
+
+generateCard();
