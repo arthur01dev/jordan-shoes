99e7f05e60ef4ae27f56355dc3c583d770924678
Arthur Rodrigues de Souza <arthurrodriguesdesouza66@gmail.com>
2024-06-27 22:24:57 -0300

    refatorando functions, e criando function pra details

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,14 @@ const botaoVoltar = document.querySelector('.voltar');
 const sectionDetalhesProduto = document.querySelector('.produto__detalhes');
 const sectionProdutos = document.querySelector('.produtos');
 
-botaoVoltar.style.display = 'none';
-sectionDetalhesProduto.style.display = 'none';
+const ocutarBotaoESecao = () => {
+    botaoVoltar.style.display = 'none';
+    sectionDetalhesProduto.style.display = 'none';
+};
 
-const formatCurrency = (number) => {
+ocutarBotaoESecao();
+
+const numberFormat = (number) => {
     return number.toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL'
@@ -24,29 +28,8 @@ const generateCard = async () => {
         let card = document.createElement('div');
         card.id = product.id;
         card.classList.add('card__produto');
-        card.innerHTML = `
-        <figure>
-            <img src="assets/images/${product.image}" alt="${product.product_name}">
-        </figure>
-        <div class="card__produto_detalhes">
-            <h4>${product.product_name}</h4>
-            <h5>${product.product_model}</h5>
-        </div>
-        <h6>${formatCurrency(product.price)}</h6>`;
-        const listaProdutos = document.querySelector('.lista__produtos');
-        listaProdutos.appendChild(card);
-
-        card.addEventListener('click', (e) => {
-            sectionProdutos.style.display = 'none';
-            botaoVoltar.style.display = 'grid';
-            sectionDetalhesProduto.style.display = 'grid';
-
-            const cardClicado = e.currentTarget;
-            const idProduto = cardClicado.id;
-            const produtoClicado = products.find(product => product.id == idProduto);
-
-            preencherDadosProduto(produtoClicado);
-        })
+        geraHtmlCards(card, product);
+        preencherCard(card, products);
     });
 };
 
@@ -54,8 +37,7 @@ generateCard();
 
 botaoVoltar.addEventListener('click', () => {
     sectionProdutos.style.display = 'flex';
-    botaoVoltar.style.display = 'none';
-    sectionDetalhesProduto.style.display = 'none';
+    ocutarBotaoESecao();
 });
 
 const preencherDadosProduto = (product) => {
@@ -67,5 +49,42 @@ const preencherDadosProduto = (product) => {
 
     document.querySelector('.detalhes h4').innerHTML = product.product_name;
     document.querySelector('.detalhes h5').innerHTML = product.product_model;
-    document.querySelector('.detalhes h6').innerHTML = formatCurrency(product.price);
+    document.querySelector('.detalhes h6').innerHTML = numberFormat(product.price);
 };
+
+const details = document.querySelector('details');
+
+details.addEventListener('toggle', () => {
+    const summary = document.querySelector('summary');
+    summary.classList.toggle('icone-expandir');
+    summary.classList.toggle('icone-recolher');
+});
+
+const preencherCard = (card, products) => {
+    card.addEventListener('click', (e) => {
+        sectionProdutos.style.display = 'none';
+        botaoVoltar.style.display = 'grid';
+        sectionDetalhesProduto.style.display = 'grid';
+
+        const cardClicado = e.currentTarget;
+        const idProduto = cardClicado.id;
+        const produtoClicado = products.find(product => product.id == idProduto);
+
+        preencherDadosProduto(produtoClicado);
+    });
+};
+
+const geraHtmlCards = (card, product) => {
+    card.innerHTML = `
+        <figure>
+            <img src="assets/images/${product.image}" alt="${product.product_name}">
+        </figure>
+        <div class="card__produto_detalhes">
+            <h4>${product.product_name}</h4>
+            <h5>${product.product_model}</h5>
+        </div>
+        <h6>${numberFormat(product.price)}</h6>`;
+
+    const listaProdutos = document.querySelector('.lista__produtos');
+    listaProdutos.appendChild(card);
+}
